f85fccd686a9bf0e52edd09cf24e1cab3f263a12
Nathan Omeri <magembenath@gmail.com>
2022-12-15 18:58:52 -0800

    Find sibling template

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_sibling - find the sibling of a node
+ * @node: pointer to the node
+ *
+ * Return: pointer to the sibling, NULL otherwise
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	binary_tree_t *sibling;
+
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	if (sibling == NULL)
+		return (NULL);
+
+	return (sibling);
+}
