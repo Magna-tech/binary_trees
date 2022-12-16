0fac8befcac1c327f94d7089cd40a75af01da7b7
Nathan Omeri <magembenath@gmail.com>
2022-12-15 19:03:09 -0800

    find uncle template

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_uncle - find the uncle of a node
+ * @node: a pointer to the node to find the uncle for
+ *
+ * Return: pointer to the uncle node, NULL otherwise or if node is null
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t uncle;
+
+	if (node == NULL)
+		return (NULL);
+	if (uncle == NULL)
+		return (NULL);
+
+	return (uncle);
+}
