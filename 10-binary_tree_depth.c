9aafc59d514f5c5a41803f61dc9ae818c5a59ea3
Nathan Omeri <magembenath@gmail.com>
2022-12-24 19:17:08 -0800

    Header file

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,17 +1,16 @@
 #include "binary_trees.h"
 /**
  * binary_tree_depth - find tree depth
- * @tree: pointer to the root of the tree
+ * @tree: pointer to the node to measure depth
  *
  * Return: depth, 0 if tree is null
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t depth;
-
 	if (tree == NULL)
 		return (0);
-	depth = 1;
-
-	return (depth);
+	size_t left_depth = binary_tree_depth(tree->left);
+	size_t right_depth = binary_tree_depth(tree->right);
+	
+	return (left_depth > right_depth) ? (left_depth + 1) : (right_depth + 1);
 }
