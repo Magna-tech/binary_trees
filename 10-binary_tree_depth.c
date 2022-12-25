8a116debf1d7b6ac44c0333782c3cb2343d5190f
Nathan Omeri <magembenath@gmail.com>
2022-12-24 19:29:22 -0800

    Depth of a binary tree

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,8 +9,8 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	size_t left_depth = binary_tree_depth(tree->left);
-	size_t right_depth = binary_tree_depth(tree->right);
+	size_t left = binary_tree_depth(tree->left);
+	size_t right = binary_tree_depth(tree->right);
 	
-	return (left_depth > right_depth) ? (left_depth + 1) : (right_depth + 1);
+	return (left > right) ? (left + 1) : (right + 1);
 }
