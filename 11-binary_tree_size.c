6034b0607ceac755580bba7d6e2a3ec0ecb2ddad
Nathan Omeri <magembenath@gmail.com>
2024-02-29 21:40:28 +0000

    Added else statement

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -9,8 +9,11 @@ size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	size_t left = binary_tree_size(tree->left);
-	size_t right = binary_tree_size(tree->right);
-
-	return (1 + left + right);
+	else
+	{
+		size_t left = binary_tree_size(tree->left);
+		size_t right = binary_tree_size(tree->right);
+		
+		return (1 + left + right);
+	}
 }
