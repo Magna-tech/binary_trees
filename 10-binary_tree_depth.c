072b3296722e86f299fabe8478fe72aca9ddeac3
Nathan Omeri <magembenath@gmail.com>
2024-02-29 21:13:32 +0000

    Calculate the depth of a given node

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -7,8 +7,14 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+	size_t depth = 0;
+
 	if (tree == NULL)
 		return (0);
-	else
-		return (1);
+	while (tree->parent != NULL)
+	{
+		depth++;
+		tree = tree->parent;
+	}
+	return (depth);
 }
