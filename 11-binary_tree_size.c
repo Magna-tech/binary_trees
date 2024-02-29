a61c861b35faf320526a5abee1e61a716ed0135c
Nathan Omeri <magembenath@gmail.com>
2024-02-29 21:41:59 +0000

    Betty fix

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -13,7 +13,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	{
 		size_t left = binary_tree_size(tree->left);
 		size_t right = binary_tree_size(tree->right);
-		
+
 		return (1 + left + right);
 	}
 }
