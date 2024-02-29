10510d9a8625055bdb8fe4e5a24afd60986a9879
Nathan Omeri <magembenath@gmail.com>
2024-02-29 22:06:40 +0000

    Style fix

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,13 +8,11 @@
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-	{
 		return (0);
-	}
+
 	if (tree->left == NULL && tree->right == NULL)
-	{
 		return (1);
-	}
+
 	size_t l_leaves = binary_tree_leaves(tree->left);
 	size_t r_leaves = binary_tree_leaves(tree->right);
 
